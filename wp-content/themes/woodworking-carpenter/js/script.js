dc438e71d5bc63c91a26f8ef50aa728d98a683ce
shourov193 <shourov193@gmail.com>
2022-03-02 16:58:22 +0600

    shourov

---
+++ wp-content/themes/woodworking-carpenter/js/script.js
@@ -0,0 +1,106 @@
+/* ===============================================
+	OWL CAROUSEL SLIDER 
+=============================================== */
+
+jQuery('document').ready(function(){
+  var owl = jQuery('.slider .owl-carousel');
+    owl.owlCarousel({
+    margin:20,
+    nav: true,
+    autoplay : true,
+    lazyLoad: true,
+    autoplayTimeout: 3000,
+    loop: false,
+    dots:false,
+    navText : ['<i class="fas fa-arrow-left"></i>','<i class="fas fa-arrow-right"></i> '],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    },
+    autoplayHoverPause : true,
+    mouseDrag: true
+  });
+});
+
+/* ===============================================
+  OWL CAROUSEL OUR CATEGORY 
+=============================================== */
+
+jQuery('document').ready(function(){
+  var owl = jQuery('#our_category .owl-carousel');
+    owl.owlCarousel({
+    margin:20,
+    nav: true,
+    autoplay : true,
+    lazyLoad: true,
+    autoplayTimeout: 3000,
+    loop: false,
+    dots:true,
+    navText: ["<div class='nav-button owl-prev'><i class='fas fa-chevron-left'></i></div>", "<div class='nav-button owl-next'><i class='fas fa-chevron-right'></i></div>"],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    },
+    autoplayHoverPause : true,
+    mouseDrag: true
+  });
+});
+
+/* ===============================================
+  OPEN CLOSE Menu
+============================================= */
+
+function woodworking_carpenter_open_menu() {
+  jQuery('button.menu-toggle').addClass('close-panal');
+  setTimeout(function(){
+    jQuery('nav#main-menu').show();
+  }, 100);
+
+  return false;
+}
+
+jQuery( "button.menu-toggle").on("click", woodworking_carpenter_open_menu);
+
+function woodworking_carpenter_close_menu() {
+  jQuery('button.close-menu').removeClass('close-panal');
+  jQuery('nav#main-menu').hide();
+}
+
+jQuery( "button.close-menu").on("click", woodworking_carpenter_close_menu);
+
+/* ===============================================
+  TRAP TAB FOCUS ON MODAL MENU
+============================================= */
+
+jQuery('button.close-menu').on('keydown', function (e) {
+  if (jQuery("this:focus") && (e.which === 9)) {
+    e.preventDefault();
+    jQuery(this).blur();
+    jQuery('button.menu-toggle').focus();
+  }
+});
+
+/* ===============================================
+  CATEGORIES  DROPDOWN
+============================================= */
+
+jQuery(document).ready(function(){
+  jQuery(".product-cat").hide();
+  jQuery("button.product-btn").click(function(){
+    jQuery(".product-cat").toggle();
+  });
+});
